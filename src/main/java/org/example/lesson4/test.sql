5ae9a5871cc3a34c25809385a51e1b87978375ca
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-01-23 19:18:00 +0200

    lesson 4

--- src/main/java/org/example/lesson4/test.sql
+++ src/main/java/org/example/lesson4/test.sql
@@ -1,3 +1,5 @@
 CREATE DATABASE company_repository;
 
 CREATE SCHEMA company_storage;
+
+DROP SCHEMA company_storage;
---
+++ src/main/java/org/example/lesson5/test.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE company_storage.company(
+    id INT,
+    name VARCHAR(128),
+    date DATE
+);
+
+--DATE TIME TIMESTAMP
+-- CHAR() VARCHAR() TEXT
+
+-- comments
